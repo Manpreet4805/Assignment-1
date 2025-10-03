451776fb8109f2237d3ed550a6949008b4c1d1c4
Manpreet4805 <mrajput4805@gmail.com>
2025-10-03 10:56:22 -0400

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get("/solutions/projects", (req, res) => {
 
 
 app.get("/solutions/projects/id-demo", (req, res) => {
-    projectData.getProjectById(3)
+    projectData.getProjectById(36)
         .then((project) => res.json(project))
         .catch((err) => res.status(404).send(err));
 });
@@ -56,6 +56,7 @@ projectData.initialize()
 
 
 module.exports = app;
+
 
 
 
