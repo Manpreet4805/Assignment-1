3953df74c8560fb9eec838f5b07ee268cb9f85cf
Manpreet4805 <mrajput4805@gmail.com>
2025-10-03 09:42:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,8 @@
 *
 * https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
 *
-* Name: Your Name Here
-* Student ID: Your Student ID Here
+* Name: Manpreet Singh
+* Student ID: 190709238
 * Date: September 30, 2025
 *
 ********************************************************************************/
@@ -16,7 +16,6 @@ const express = require('express');
 const projectData = require('./modules/projects');
 
 const app = express();
-const HTTP_PORT = process.env.PORT || 3000;
 
 // ROUTES
 
@@ -34,7 +33,6 @@ app.get("/solutions/projects", (req, res) => {
 
 // Route to return a demo project by ID
 app.get("/solutions/projects/id-demo", (req, res) => {
-    // You can use any known valid ID from your project data, like 3
     projectData.getProjectById(3)
         .then((project) => res.json(project))
         .catch((err) => res.status(404).send(err));
@@ -42,19 +40,19 @@ app.get("/solutions/projects/id-demo", (req, res) => {
 
 // Route to return projects by sector (demo)
 app.get("/solutions/projects/sector-demo", (req, res) => {
-    // Try sectors like "agriculture", "land", "industry"
     projectData.getProjectsBySector("agriculture")
         .then((projects) => res.json(projects))
         .catch((err) => res.status(404).send(err));
 });
 
-// Initialize project data before starting server
+// Initialize project data
 projectData.initialize()
     .then(() => {
-        app.listen(HTTP_PORT, () => {
-            console.log(`Server is running on http://localhost:${HTTP_PORT}`);
-        });
+        console.log("Data initialized successfully");
     })
     .catch((err) => {
         console.error("Failed to initialize data:", err);
     });
+
+// Export app instead of listening (for Vercel)
+module.exports = app;
