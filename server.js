93a150d085d81a927c22733adf9ec8c4b3b31a57
Manpreet4805 <mrajput4805@gmail.com>
2025-10-03 10:25:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -17,35 +17,35 @@ const projectData = require('./modules/projects.js');
 
 const app = express();
 
-// ROUTES
 
-// Root route
+
+
 app.get("/", (req, res) => {
     res.send("Assignment 2: Manpreet Singh - 190709238");
 });
 
-// Route to return all projects
+
 app.get("/solutions/projects", (req, res) => {
     projectData.getAllProjects()
         .then((data) => res.json(data))
         .catch((err) => res.status(500).send(err));
 });
 
-// Route to return a demo project by ID
+
 app.get("/solutions/projects/id-demo", (req, res) => {
     projectData.getProjectById(3)
         .then((project) => res.json(project))
         .catch((err) => res.status(404).send(err));
 });
 
-// Route to return projects by sector (demo)
+
 app.get("/solutions/projects/sector-demo", (req, res) => {
     projectData.getProjectsBySector("agriculture")
         .then((projects) => res.json(projects))
         .catch((err) => res.status(404).send(err));
 });
 
-// Initialize project data
+
 projectData.initialize()
     .then(() => {
         console.log("Data initialized successfully");
@@ -54,6 +54,7 @@ projectData.initialize()
         console.error("Failed to initialize data:", err);
     });
 
-// Export app instead of listening (for Vercel)
+
 module.exports = app;
+
 
